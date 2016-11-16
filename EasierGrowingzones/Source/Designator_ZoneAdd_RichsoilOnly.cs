1888e2c6ce036292fa9ebe24cb3b17c4411d0ef6
PaulRitter <ritter.paul1@googlemail.com>
2016-11-16 13:29:21 +0100

    added soil selector

--- EasierGrowingzones/Source/Designator_ZoneAdd_RichsoilOnly.cs
+++ EasierGrowingzones/Source/Designator_ZoneAdd_RichsoilOnly.cs
@@ -1,26 +1,29 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
 namespace RimWorld
 {
-    public class Designator_ZoneAdd_RichsoilOnly : Designator_ZoneAdd
+    public class Designator_ZoneAdd_CostumSoilOnly : Designator_ZoneAdd
     {
+        private TerrainDef curDef = TerrainDefOf.Soil;
+
         protected override string NewZoneLabel
         {
             get
             {
-                return "RichsoilOnlyZone".Translate();
+                return "CostumSoilOnlyZone".Translate(curDef.label.CapitalizeFirst());
             }
         }
 
-        public Designator_ZoneAdd_RichsoilOnly()
+        public Designator_ZoneAdd_CostumSoilOnly()
         {
             this.zoneTypeToPlace = typeof(Zone_Growing);
-            this.defaultLabel = "RichsoilOnlyZone".Translate();
-            this.defaultDesc = "DesignatorRichsoilOnlyZoneDesc".Translate();
+            this.defaultLabel = "CostumSoilOnlyZone".Translate(curDef.label.CapitalizeFirst());
+            this.defaultDesc = "DesignatorCostumSoilZoneDesc".Translate(curDef.label.CapitalizeFirst());
             this.icon = ContentFinder<Texture2D>.Get("UI/Designators/ZoneCreate_Growing", true);
-            this.tutorTag = "test";
+            this.tutorTag = "";
         }
 
         public override AcceptanceReport CanDesignateCell(IntVec3 c)
@@ -29,7 +32,7 @@ namespace RimWorld
             {
                 return false;
             }
-            if (Find.FertilityGrid.FertilityAt(c) < 1.4f)
+            if (Find.TerrainGrid.TerrainAt(c) != curDef)
             {
                 return false;
             }
@@ -40,6 +43,46 @@ namespace RimWorld
         {
             PlayerKnowledgeDatabase.KnowledgeDemonstrated(ConceptDefOf.GrowingFood, KnowledgeAmount.Total);
             return new Zone_Growing();
+        }
+
+        public override void ProcessInput(Event ev)
+        {
+            if (!base.CheckCanInteract())
+            {
+                return;
+            }
+            List<FloatMenuOption> list = new List<FloatMenuOption>();
+            IEnumerable<TerrainDef> enumerable;
+            enumerable = from floor in DefDatabase<TerrainDef>.AllDefs
+                         where floor.fertility >= 0.4
+                         select floor;
+            enumerable = from x in enumerable
+                         orderby x.fertility descending
+                         select x;
+            foreach (TerrainDef current in enumerable)
+            {
+                string labelCap = current.label.CapitalizeFirst(); ;
+                list.Add(new FloatMenuOption(labelCap, delegate
+                {
+                    this.ProcessInput(ev);
+                    DesignatorManager.Select(this);
+                    this.curDef = TerrainDef.Named(current.defName);
+                }, MenuOptionPriority.Medium, null, null, 0f, null)
+                {
+                    tutorTag = "SelectStuff-" + current.defName
+                });
+            }
+            if (list.Count == 0)
+            {
+                Messages.Message("ERR_No_TerrainDefs".Translate(), MessageSound.RejectInput);
+            }
+            else
+            {
+                FloatMenu floatMenu = new FloatMenu(list);
+                floatMenu.vanishIfMouseDistant = true;
+                Find.WindowStack.Add(floatMenu);
+                DesignatorManager.Select(this);
+            }
         }
     }
 }
